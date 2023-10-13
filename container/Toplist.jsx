1fdf81ffed541d848976e5f30414fdd936a4adcc
Lakshay goyal <akshaybuster5280@gmail.com>
2023-10-13 20:23:03 +0530

    added a mising title

--- container/Toplist.jsx
+++ container/Toplist.jsx
@@ -8,36 +8,39 @@ const Toplist = () => {
       <div className="list">
         <Topcard type="hospital" />
         <Topcard type="doctor" />
+          <h2 className="servt">Servicies</h2>
         <div className="serv_holder">
           {servicies.map((serv) => (
-            <div className="serv_card" key={serv}>
-              <div className="serv_card-image">
-                <Image
-                  src={serv.img}
-                  alt={serv.title}
-                  width={120}
-                  height={30}
-                />
-              </div>
-              <div className="serv_card-box">
-                <div className="serv_card-content">
-                  <div>
-                    <h2>{serv.title}</h2>
+            <>
+              <div className="serv_card" key={serv}>
+                <div className="serv_card-image">
+                  <Image
+                    src={serv.img}
+                    alt={serv.title}
+                    width={120}
+                    height={30}
+                  />
+                </div>
+                <div className="serv_card-box">
+                  <div className="serv_card-content">
+                    <div>
+                      <h2>{serv.title}</h2>
+                    </div>
+                    <div>
+                      <p>{serv.desc}</p>
+                    </div>
                   </div>
-                  <div>
-                    <p>{serv.desc}</p>
+                  <div className="serv_card-content2">
+                    <div>
+                      <a href="#">{serv.know}</a>
+                    </div>
+                    <button>
+                      <a href="#">{serv.btn}</a>
+                    </button>
                   </div>
                 </div>
-                <div className="serv_card-content2">
-                  <div>
-                    <a href="#">{serv.know}</a>
-                  </div>
-                  <button>
-                    <a href="#">{serv.btn}</a>
-                  </button>
-                </div>
               </div>
-            </div>
+            </>
           ))}
         </div>
       </div>
@@ -46,3 +49,4 @@ const Toplist = () => {
 };
 
 export default Toplist;
+    
